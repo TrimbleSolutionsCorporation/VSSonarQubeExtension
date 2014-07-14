2f0b363758b75b9f7f242780be508bef649daf7e
jmecosta <jmecosta@gmail.com>
2014-07-14 16:41:26 +0300

    #35 add assembly patcher and prepare for new release

--- SonarLocalAnalyser/AssemblyInfo.fs
+++ SonarLocalAnalyser/AssemblyInfo.fs
@@ -1,39 +1,41 @@
 ﻿// --------------------------------------------------------------------------------------------------------------------
-// <copyright file="AssemblyInfo.cs" company="Copyright © 2013 Tekla Corporation. Tekla is a Trimble Company">
-//     Copyright (C) 2013 [Jorge Costa, Jorge.Costa@tekla.com]
+// <copyright file="AssemblyInfo.fs" company="Trimble Navigation Limited">
+//     Copyright (C) 2013 [Jorge Costa, Jorge.Costa@trimble.com]
 // </copyright>
 // --------------------------------------------------------------------------------------------------------------------
-// This program is free software; you can redistribute it and/or modify it under the terms of the GNU Lesser General Public License
-// as published by the Free Software Foundation; either version 3 of the License, or (at your option) any later version.
+// This program is free software; you can redistribute it and/or modify it under the terms of the GNU General Public License
+// as published by the Free Software Foundation; either version 2 of the License, or (at your option) any later version.
 //
 // This program is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; without even the implied warranty
-// of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more details. 
-// You should have received a copy of the GNU Lesser General Public License along with this program; if not, write to the Free
+// of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU General Public License for more details. 
+// You should have received a copy of the GNU General Public License along with this program; if not, write to the Free
 // Software Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA 02111-1307 USA
 // --------------------------------------------------------------------------------------------------------------------
+namespace SonarRestService
 
-using System.Reflection;
-using System.Runtime.InteropServices;
+open System.Reflection
+open System.Runtime.CompilerServices
+open System.Runtime.InteropServices
 
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("ExtensionHelpers")]
-[assembly: AssemblyDescription("")]
-[assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("Tekla Corporation. Tekla is a Trimble Company")]
-[assembly: AssemblyProduct("ExtensionHelpers")]
-[assembly: AssemblyCopyright("Copyright © 2013 Tekla Corporation. Tekla is a Trimble Company")]
-[assembly: AssemblyTrademark("")]
-[assembly: AssemblyCulture("")]
+[<assembly: AssemblyTitle("SonarLocalAnalyser")>]
+[<assembly: AssemblyDescription("")>]
+[<assembly: AssemblyConfiguration("")>]
+[<assembly: AssemblyCompany("Tekla Corporation. Tekla is a Trimble Company")>]
+[<assembly: AssemblyProduct("SonarLocalAnalyser")>]
+[<assembly: AssemblyCopyright("Copyright © 2013 Tekla Corporation. Tekla is a Trimble Company")>]
+[<assembly: AssemblyTrademark("")>]
 
 // Setting ComVisible to false makes the types in this assembly not visible 
 // to COM components.  If you need to access a type in this assembly from 
 // COM, set the ComVisible attribute to true on that type.
-[assembly: ComVisible(false)]
+[<assembly: ComVisible(false)>]
 
 // The following GUID is for the ID of the typelib if this project is exposed to COM
-[assembly: Guid("18eaa6e3-bd72-4da7-836d-26bfaa9022b3")]
+[<assembly: Guid("ccb75517-2822-48c0-86c0-c66614328305")>]
+
 
 // Version information for an assembly consists of the following four values:
 //
@@ -45,5 +47,7 @@ using System.Runtime.InteropServices;
 // You can specify all the values or you can default the Build and Revision Numbers 
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.3.3.1")]
-[assembly: AssemblyFileVersion("1.3.3.1")]
+[<assembly: AssemblyVersion("1.3.3.1")>]
+[<assembly: AssemblyFileVersion("1.3.3.1")>]
+
+()
--- SonarRestService/AssemblyInfo.fs
+++ SonarRestService/AssemblyInfo.fs
@@ -36,6 +36,7 @@ open System.Runtime.InteropServices
 // The following GUID is for the ID of the typelib if this project is exposed to COM
 [<assembly: Guid("ccb75517-2822-48c0-86c0-c66614328305")>]
 
+
 // Version information for an assembly consists of the following four values:
 //
 //      Major Version
@@ -46,7 +47,7 @@ open System.Runtime.InteropServices
 // You can specify all the values or you can default the Build and Revision Numbers 
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[<assembly: AssemblyVersion("1.3.2.1")>]
-[<assembly: AssemblyFileVersion("1.3.2.1")>]
+[<assembly: AssemblyVersion("1.3.3.1")>]
+[<assembly: AssemblyFileVersion("1.3.3.1")>]
 
 ()
