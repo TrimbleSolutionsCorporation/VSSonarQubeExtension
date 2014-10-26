5d61ed7f66c7050041f456a162d8daf615904887
jmecosta <jmecosta@gmail.com>
2014-10-26 23:48:58 +0200

    #51 fix async observable collection

--- VSSonarExtensionUi/Helpers/AsyncObservableCollection.cs
+++ VSSonarExtensionUi/Helpers/AsyncObservableCollection.cs
@@ -6,130 +6,51 @@
 //   The async observable collection.
 // </summary>
 // --------------------------------------------------------------------------------------------------------------------
+
 namespace VSSonarExtensionUi.Helpers
 {
     using System;
-    using System.Collections.Generic;
     using System.Collections.ObjectModel;
-    using System.Threading;
+    using System.Collections.Specialized;
+    using System.Windows.Threading;
 
     /// <summary>
-    /// The async observable collection.
+    ///     The async observable collection.
     /// </summary>
     /// <typeparam>
     ///     <name>T</name>
     /// </typeparam>
     public class AsyncObservableCollection<T> : ObservableCollection<T>
     {
-        #region Fields
+        #region Public Events
 
-        /// <summary>
-        /// The _synchronization context.
-        /// </summary>
-        private readonly SynchronizationContext synchronizationContext = SynchronizationContext.Current;
-
-        #endregion
-
-        #region Constructors and Destructors
-
-        /// <summary>
-        /// Initializes a new instance of the <see cref="AsyncObservableCollection{T}"/> class.
-        /// </summary>
-        public AsyncObservableCollection()
-        {
-        }
-
-        /// <summary>
-        /// Initializes a new instance of the <see cref="AsyncObservableCollection{T}"/> class.
-        /// </summary>
-        /// <param name="list">
-        /// The list.
-        /// </param>
-        public AsyncObservableCollection(IEnumerable<T> list)
-            : base(list)
-        {
-        }
+        public override event NotifyCollectionChangedEventHandler CollectionChanged;
 
         #endregion
 
         #region Methods
 
-        /// <summary>
-        /// The clear items.
-        /// </summary>
-        protected override void ClearItems()
+        protected override void OnCollectionChanged(NotifyCollectionChangedEventArgs e)
         {
-            this.ExecuteOnSyncContext(() => base.ClearItems());
-        }
-
-        /// <summary>
-        /// The insert item.
-        /// </summary>
-        /// <param name="index">
-        /// The index.
-        /// </param>
-        /// <param name="item">
-        /// The item.
-        /// </param>
-        protected override void InsertItem(int index, T item)
-        {
-            this.ExecuteOnSyncContext(() => base.InsertItem(index, item));
-        }
-
-        /// <summary>
-        /// The move item.
-        /// </summary>
-        /// <param name="oldIndex">
-        /// The old index.
-        /// </param>
-        /// <param name="newIndex">
-        /// The new index.
-        /// </param>
-        protected override void MoveItem(int oldIndex, int newIndex)
-        {
-            this.ExecuteOnSyncContext(() => base.MoveItem(oldIndex, newIndex));
-        }
-
-        /// <summary>
-        /// The remove item.
-        /// </summary>
-        /// <param name="index">
-        /// The index.
-        /// </param>
-        protected override void RemoveItem(int index)
-        {
-            this.ExecuteOnSyncContext(() => base.RemoveItem(index));
-        }
-
-        /// <summary>
-        /// The set item.
-        /// </summary>
-        /// <param name="index">
-        /// The index.
-        /// </param>
-        /// <param name="item">
-        /// The item.
-        /// </param>
-        protected override void SetItem(int index, T item)
-        {
-            this.ExecuteOnSyncContext(() => base.SetItem(index, item));
-        }
-
-        /// <summary>
-        /// The execute on sync context.
-        /// </summary>
-        /// <param name="action">
-        /// The action.
-        /// </param>
-        private void ExecuteOnSyncContext(Action action)
-        {
-            if (SynchronizationContext.Current == this.synchronizationContext)
+            NotifyCollectionChangedEventHandler CollectionChanged = this.CollectionChanged;
+            if (CollectionChanged != null)
             {
-                action();
-            }
-            else
-            {
-                this.synchronizationContext.Send(_ => action(), null);
+                foreach (NotifyCollectionChangedEventHandler nh in CollectionChanged.GetInvocationList())
+                {
+                    var dispObj = nh.Target as DispatcherObject;
+                    if (dispObj != null)
+                    {
+                        Dispatcher dispatcher = dispObj.Dispatcher;
+                        if (dispatcher != null && !dispatcher.CheckAccess())
+                        {
+                            dispatcher.BeginInvoke(
+                                (Action)(() => nh.Invoke(this, new NotifyCollectionChangedEventArgs(NotifyCollectionChangedAction.Reset))),
+                                DispatcherPriority.DataBind);
+                            continue;
+                        }
+                    }
+                    nh.Invoke(this, e);
+                }
             }
         }
 
