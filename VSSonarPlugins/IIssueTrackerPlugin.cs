b35c47ac0a7236c49771314bccc77c07f0999f52
jmecosta <jmecosta@gmail.com>
2015-07-22 15:20:36 +0300

    #121 add issue tracking interface and source plugin

---
+++ VSSonarPlugins/IIssueTrackerPlugin.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using VSSonarPlugins.Types;
+
+namespace VSSonarPlugins
+{
+    interface ISourceVersionPlugin
+    {
+        IList<string> GetHistory(Resource item);
+    }
+
+    interface IIssueTrackerPlugin
+    {
+        void AttachToExistentDefect(IList<Issue> issues, int defectId);
+        void CreateDefect(IList<Issue> issues);
+        void GetIssuesFromResource(Resource item, ISourceVersionPlugin versionPlugin);
+    }
+}
