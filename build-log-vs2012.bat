01f7311028dfa5909165b0ad891d2a36a0bd33bf
jmecosta <jmecosta@gmail.com>
2016-01-05 22:58:54 +0200

    change build scripts

--- build-log-vs2012.bat
+++ build-log-vs2012.bat
@@ -2,5 +2,5 @@
 
 call "C:\Program Files (x86)\Microsoft Visual Studio 14.0\Common7\Tools\vsvars32.bat"
 call "C:\Program Files (x86)\Microsoft Visual Studio 14.0\Common7\Tools\VsDevCmd.bat"
-msbuild VSSonarQubeExtension2012.sln /p:VisualStudioVersion=12.0 /p:Vsix=Vs2012 /p:VsFolder=vs12 /p:SolutionFile=VSSonarQubeExtension2012.sln /p:Configuration=Release /v:diagnostic > buildlog2012.txt
+msbuild BuildExtension.msbuild /p:VSVersion=11.0 /p:Vsix=Vs2012 /p:VsFolder=vs12 /p:SolutionFile=VSSonarQubeExtension2012.sln /p:Configuration=Release /v:diagnostic > buildlog2012.txt
 
--- build.bat
+++ build.bat
@@ -3,12 +3,12 @@
 
 call "C:\Program Files (x86)\Microsoft Visual Studio 14.0\Common7\Tools\vsvars32.bat"
 call "C:\Program Files (x86)\Microsoft Visual Studio 14.0\Common7\Tools\VsDevCmd.bat"
-msbuild BuildExtension.msbuild /p:VisualStudioVersion=14.0 /p:Vsix=Vs2015 /p:VsFolder=vs15 /p:SolutionFile=VSSonarQubeExtension2015.sln /p:Configuration=Release
-msbuild VSSonarQubeExtension2012.sln /p:VisualStudioVersion=12.0 /p:Vsix=Vs2012 /p:VsFolder=vs12 /p:SolutionFile=VSSonarQubeExtension2012.sln /p:Configuration=Release
+msbuild BuildExtension.msbuild /p:VsVersion=14.0 /p:Vsix=Vs2015 /p:VsFolder=vs15 /p:SolutionFile=VSSonarQubeExtension2015.sln /p:Configuration=Release
+msbuild BuildExtension.msbuild /p:VSVersion=11.0 /p:Vsix=Vs2012 /p:VsFolder=vs12 /p:SolutionFile=VSSonarQubeExtension2012.sln /p:Configuration=Release
 
 call "C:\Program Files (x86)\Microsoft Visual Studio 12.0\Common7\Tools\vsvars32.bat"
 call "C:\Program Files (x86)\Microsoft Visual Studio 12.0\Common7\Tools\VsDevCmd.bat"
-msbuild VSSonarQubeExtension2013.sln /p:VisualStudioVersion=12.0 /p:Vsix=Vs2013 /p:VsFolder=vs13 /p:SolutionFile=VSSonarQubeExtension2013.sln /p:Configuration=Release
+msbuild BuildExtension.msbuild /p:VsVersion=12.0 /p:Vsix=Vs2013 /p:VsFolder=vs13 /p:SolutionFile=VSSonarQubeExtension2013.sln /p:Configuration=Release
 
 
 
