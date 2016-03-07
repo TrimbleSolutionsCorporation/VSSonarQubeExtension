128be31e08691f502ac2237f47cf18e4fe0e51c1
jmecosta <jmecosta@gmail.com>
2016-03-07 20:19:53 +0200

    #230 pass fromSave to plugins

--- build-log-vs2012.bat
+++ build-log-vs2012.bat
@@ -1,5 +1,5 @@
 @echo on
-
+nuget restore VSSonarQubeExtension2012.sln
 call "C:\Program Files (x86)\Microsoft Visual Studio 14.0\Common7\Tools\vsvars32.bat"
 call "C:\Program Files (x86)\Microsoft Visual Studio 14.0\Common7\Tools\VsDevCmd.bat"
 msbuild BuildExtension.msbuild /p:VSVersion=11.0 /p:Vsix=Vs2012 /p:VsFolder=vs12 /p:SolutionFile=VSSonarQubeExtension2012.sln /p:Configuration=Release /v:diagnostic > buildlog2012.txt
--- build-log-vs2015.bat
+++ build-log-vs2015.bat
@@ -1,5 +1,5 @@
 @echo on
-
+nuget restore VSSonarQubeExtension2015.sln
 call "C:\Program Files (x86)\Microsoft Visual Studio 14.0\Common7\Tools\vsvars32.bat"
 call "C:\Program Files (x86)\Microsoft Visual Studio 14.0\Common7\Tools\VsDevCmd.bat"
 msbuild BuildExtension.msbuild /p:VisualStudioVersion=14.0 /p:Vsix=Vs2015 /p:VsVersion=14.0 /p:VsFolder=vs15 /p:SolutionFile=VSSonarQubeExtension2015.sln /p:Configuration=Release > buildlog2015.txt
