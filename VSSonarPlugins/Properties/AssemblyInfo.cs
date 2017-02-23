2febd8926c3f7fcf8688ae6739383485f6fbbda3
jmecosta <jmecosta@gmail.com>
2017-02-23 23:18:21 +0200

    #260 initial support 6.3

--- VSSonarPlugins/Properties/AssemblyInfo.cs
+++ VSSonarPlugins/Properties/AssemblyInfo.cs
@@ -45,4 +45,4 @@ using System.Runtime.InteropServices;
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
 [assembly: AssemblyVersion("6.0.0.0")]
-[assembly: AssemblyFileVersion("6.0.5.0")]
+[assembly: AssemblyFileVersion("6.0.6.0")]
