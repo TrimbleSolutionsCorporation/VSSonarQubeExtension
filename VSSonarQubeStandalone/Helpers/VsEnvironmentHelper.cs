b1d11d0c27c55a502c5afcca286bb80cc6f47d06
jmecosta <jmecosta@gmail.com>
2019-08-29 10:39:09 +0300

    latest changes

---
+++ VSSonarQubeStandalone/Helpers/VsEnvironmentHelper.cs
@@ -0,0 +1,179 @@
+﻿using System;
+using System.Diagnostics;
+using Microsoft.CodeAnalysis;
+using VSSonarPlugins;
+using VSSonarPlugins.Types;
+
+namespace VSSonarQubeStandalone.Helpers
+{
+    /// <summary>
+    /// dummy class
+    /// </summary>
+    /// <seealso cref="VSSonarPlugins.IVsEnvironmentHelper" />
+    public class VsEnvironmentHelper : IVsEnvironmentHelper
+    {
+        public string ActiveConfiguration()
+        {
+            throw new NotImplementedException();
+        }
+
+        public string ActiveFileFullPath()
+        {
+            throw new NotImplementedException();
+        }
+
+        public string ActivePlatform()
+        {
+            throw new NotImplementedException();
+        }
+
+        public string ActiveProjectFileFullPath()
+        {
+            throw new NotImplementedException();
+        }
+
+        public string ActiveProjectName()
+        {
+            throw new NotImplementedException();
+        }
+
+        public string ActiveSolutionName()
+        {
+            throw new NotImplementedException();
+        }
+
+        public string ActiveSolutionPath()
+        {
+            throw new NotImplementedException();
+        }
+
+        public bool AreWeRunningInVisualStudio()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void ClearDiffFile(string localFileName, string serverFileName)
+        {
+            throw new NotImplementedException();
+        }
+
+        public string CurrentSelectedDocumentLanguage()
+        {
+            throw new NotImplementedException();
+        }
+
+        public bool DoIHaveAdminRights()
+        {
+            throw new NotImplementedException();
+        }
+
+        public string EvaluatedValueForIncludeFile(string msbuildProjectFile, string filePath)
+        {
+            throw new NotImplementedException();
+        }
+
+        public string GetCurrentDocumentInView()
+        {
+            throw new NotImplementedException();
+        }
+
+        public Solution GetCurrentRoslynSolution()
+        {
+            throw new NotImplementedException();
+        }
+
+        public string GetFileRealPathForSolution(string fileInView)
+        {
+            throw new NotImplementedException();
+        }
+
+        public string GetGuidForProject(string projectPath, string solutionPath)
+        {
+            throw new NotImplementedException();
+        }
+
+        public VsProjectItem GetProjectByGuidInSolution(string projectGuid, string solutionPath)
+        {
+            throw new NotImplementedException();
+        }
+
+        public VsProjectItem GetProjectByNameInSolution(string projectName, string solutionPath)
+        {
+            throw new NotImplementedException();
+        }
+
+        public string GetProperFilePathCapitalization(string filename)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void NavigateToResource(string url)
+        {
+            Process.Start(url);
+        }
+
+        public void OpenResourceInVisualStudio(string filename, int line, string editorCommandExec = "notepad")
+        {
+            throw new NotImplementedException();
+        }
+
+        public void OpenResourceInVisualStudio(string workfolder, string filename, int line, string editorCommandExec = "notepad")
+        {
+            throw new NotImplementedException();
+        }
+
+        public string ReadSavedOption(string category, string page, string item)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void RestartVisualStudio()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void SetCurrentDocumentInView(string fullName)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void ShowSourceDiff(string resourceInEditorTxt, string documentInViewPath)
+        {
+            throw new NotImplementedException();
+        }
+
+        public VsFileItem VsFileItem(string filename, Resource associatedProject, Resource fileResource)
+        {
+            throw new NotImplementedException();
+        }
+
+        public VsFileItem VsFileItem(string fullPath, string projectFullPath, Resource associatedProject, Resource fileResource)
+        {
+            throw new NotImplementedException();
+        }
+
+        public VsProjectItem VsProjectItem(string projectFileName, Resource associatedProject)
+        {
+            throw new NotImplementedException();
+        }
+
+        public string VsVersion()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void WriteDefaultOption(string sonarOptions, string communityOptions, string item, string value)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void WriteOption(string category, string page, string item, string value)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void WriteToVisualStudioOutput(string errorMessage)
+        {
+        }
+    }
+}
---
+++ VSSonarQubeStandalone/Helpers/VsStatusBarDummy.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using VSSonarPlugins;
+
+namespace VSSonarQubeStandalone.Helpers
+{
+    class VsStatusBarDummy : IVSSStatusBar
+    {
+        public void DisplayAndShowIcon(string message)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void DisplayAndShowProgress(string message)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void DisplayMessage(string message)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void ShowIcons()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
